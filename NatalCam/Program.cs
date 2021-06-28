d44dd860bc3c4c315b5e2807eb971bd695f60eec
trevisharp <38115923+trevisharp@users.noreply.github.com>
2021-06-28 09:06:10 -0300

    Commit Inicial

---
+++ NatalCam/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+Application.SetHighDpiMode(HighDpiMode.SystemAware);
+Application.EnableVisualStyles();
+Application.SetCompatibleTextRenderingDefault(false);
+
+var form = new Form();
+
+Application.Run(form);
