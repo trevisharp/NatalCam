ddfcef5b11ac09eac060cbb5971de92590422f33
trevisharp <38115923+trevisharp@users.noreply.github.com>
2021-06-28 11:09:53 -0300

    Adição da classe de extrator de silhueta com implementação de blur

--- NatalCam/Program.cs
+++ NatalCam/Program.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using NatalCam;
 using OpenCvSharp;
+using System.Drawing;
+using System.Windows.Forms;
 using OpenCvSharp.Extensions;
 
 Application.SetHighDpiMode(HighDpiMode.SystemAware);
@@ -15,6 +12,7 @@ var form = new Form();
 var capture = new VideoCapture(0);
 var pb = new PictureBox();
 var timer = new Timer();
+SilhouetteExtractor silhouette = null;
 Graphics g = null;
 Bitmap bmp = null;
 Mat mat = null;
@@ -34,18 +32,21 @@ form.KeyDown += (sender, e) =>
     else if (e.KeyCode == Keys.S)
     {
         capture.Read(bg);
+        silhouette.Background = bg;
     }
 };
 
 timer.Tick += delegate
 {
     capture.Read(mat);
-    g.DrawImage(mat.ToBitmap(), new Rectangle(0, 0, pb.Width, pb.Height));
+    g.DrawImage(silhouette.Extract(mat).ToBitmap(),
+        new Rectangle(0, 0, pb.Width, pb.Height));
     pb.Refresh();
 };
 
 form.Load += delegate
 {
+    silhouette = new SilhouetteExtractor();
     bmp = new Bitmap(pb.Width, pb.Height);
     mat = new Mat(new int[] { 
         capture.FrameWidth, capture.FrameHeight
@@ -53,6 +54,8 @@ form.Load += delegate
     bg = new Mat(new int[] { 
         capture.FrameWidth, capture.FrameHeight
     }, MatType.CV_8U);
+    capture.Read(bg);
+    silhouette.Background = bg;
     g = Graphics.FromImage(bmp);
     pb.Image = bmp;
     timer.Start();
---
+++ NatalCam/SilhouetteExtractor.cs
@@ -0,0 +1,42 @@
+﻿using OpenCvSharp;
+
+namespace NatalCam
+{
+    public class SilhouetteExtractor
+    {
+        private Mat background = null;
+        private Mat mbbackground = null;
+        private Mat rmat = null;
+
+        public Mat Background
+        {
+            get => background;
+            set
+            {
+                background = value;
+                if (mbbackground == null)
+                {
+                    mbbackground = new Mat(new int[]
+                    {
+                        background.Width, background.Height
+                    }, MatType.CV_8U);
+                }
+
+                Cv2.MedianBlur(background, mbbackground, 15);
+            }
+        }
+
+        public Mat Extract(Mat mat)
+        {
+            rmat ??=
+                rmat = new Mat(new int[]
+                {
+                    mat.Width, mat.Height
+                }, MatType.CV_8U);
+
+            Cv2.MedianBlur(mat, rmat, 15);
+
+            return rmat;
+        }
+    }
+}
